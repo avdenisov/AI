b1b14b5ad9db9736bf520222691ad2dae3fe03c2
Alexey Denisov <a_denisov@hotmail.com>
2018-03-24 16:48:32 +0300

    Build an Alexa History Skill @ Udacity's AI
    Project: Build an Alexa History Skill @ Udacity's Artificial Intelligence nanodegree

---
+++ AIND-VUI-Alexa/src/facts.js
@@ -0,0 +1,17 @@
+// TODO Add more or replace entirely the facts below for your chosen history domain
+// Include at least 10 distinct facts with a 4-digit year included in the text for each
+module.exports = {
+    "FACTS_EN": [
+        "In 1991, the Soviet Union is dismantled and Russia becomes an independent federation.",
+        "In 1917, revolutionaries overthrew the Russian government.",
+        "In 1957, the Soviet Union launches Sputnik, the first artificial space object.",
+        "In 1961, Yuri Gagarin becomes the first man in the space.",
+        "In 1867, Russia sold Alaska to the United States.",
+        "Peter the Great became Czar of Russia in 1682",
+        "The Crimean War broke out between Russia and the Ottoman Empire in 1853.",
+        "Ivan IV (Ivan the Terrible) became the first Czar of Russia in 1547.",
+        "In 1812, Napoleon Bonaparte led troops into Russia but was defeated.",
+        "In 1945, the Soviet Union defeated Germany.",
+        "In 1962, the Cuban Missile Crisis happened."      
+    ]
+}
---
+++ AIND-VUI-Alexa/src/index.js
@@ -0,0 +1,150 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+var APP_ID = undefined;  // can be replaced with your app ID if publishing
+var facts = require('./facts');
+var GET_FACT_MSG_EN = [
+    "Here is the fact about the Russian history: ",
+    "The following events happened in Russia: ",
+    "An event from the history of Russia: ",
+    "An intersting event from the Russian history: ",
+    "An interesting fact about the Russian history: "
+]
+// Test hooks - do not remove!
+exports.GetFactMsg = GET_FACT_MSG_EN;
+var APP_ID_TEST = "mochatest";  // used for mocha tests to prevent warning
+// end Test hooks
+/*
+    TODO (Part 2) add messages needed for the additional intent
+    TODO (Part 3) add reprompt messages as needed
+*/
+
+var languageStrings = {
+    "en": {
+        "translation": {
+            "FACTS": facts.FACTS_EN,
+            "SKILL_NAME": "Russian history facts",  // OPTIONAL change this to a more descriptive name
+            "GET_FACT_MESSAGE": GET_FACT_MSG_EN,
+            "HELP_MESSAGE": "You can say tell me a fact, or, you can say exit... What can I help you with?",
+            "HELP_REPROMPT": "What can I help you with?",
+            "FACT_REPROMPT": " What year are you interested in in the Russian history?",
+            "STOP_MESSAGE": "Goodbye!"
+        }
+    }
+};
+
+exports.handler = function (event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // set a test appId if running the mocha local tests
+    if (event.session.application.applicationId == "mochatest") {
+        alexa.appId = APP_ID_TEST
+    }
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+/*
+    TODO (Part 2) add an intent for specifying a fact by year named 'GetNewYearFactIntent'
+    TODO (Part 2) provide a function for the new intent named 'GetYearFact' 
+        that emits a randomized fact that includes the year requested by the user
+        - if such a fact is not available, tell the user this and provide an alternative fact.
+    TODO (Part 3) Keep the session open by providing the fact with :askWithCard instead of :tellWithCard
+        - make sure the user knows that they need to respond
+        - provide a reprompt that lets the user know how they can respond
+    TODO (Part 3) Provide a randomized response for the GET_FACT_MESSAGE
+        - add message to the array GET_FACT_MSG_EN
+        - randomize this starting portion of the response for conversational variety
+*/
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random fact from the facts list
+        // Use this.t() to get corresponding language data
+        var factArr = this.t('FACTS');
+        var randomFact = randomPhrase(factArr);
+
+        // Create speech output
+        var randomFactResponse = randomPhrase(this.t("GET_FACT_MESSAGE"));
+        var speechOutput = randomFactResponse + randomFact + this.t("FACT_REPROMPT");
+        // this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomFact)
+        this.emit(':askWithCard', speechOutput, this.t("FACT_REPROMPT"), this.t("SKILL_NAME"), randomFact)
+    },
+    'GetNewYearFactIntent': function () {
+        //TODO your code here
+        var factArr = this.t('FACTS');
+        var yearToFind = this.event.request.intent.slots.FACT_YEAR.value;
+        var factToTell;
+        switch (yearToFind) {
+            case '1991' : 
+                factToTell = factArr[0];
+                break;
+            case '1917' : 
+                factToTell = factArr[1];
+                break;
+            case '1957' : 
+                factToTell = factArr[2];
+                break;
+            case '1961' : 
+                factToTell = factArr[3];
+                break;
+            case '1867' : 
+                factToTell = factArr[4];
+                break;
+            case '1682' : 
+                factToTell = factArr[5];
+                break;
+            case '1853' : 
+                factToTell = factArr[6];
+                break;
+            case '1547' : 
+                factToTell = factArr[7];
+                break;
+            case '1812' : 
+                factToTell = factArr[8];
+                break;
+            case '1945' : 
+                factToTell = factArr[9];
+                break;
+            case '1962' : 
+                factToTell = factArr[10];
+                break;
+            default : 
+                factToTell = "No facts for the year you mentioned, but for example "+ randomPhrase(factArr);
+        }
+        
+        var randomFactResponse = randomPhrase(this.t("GET_FACT_MESSAGE"));
+        var speechOutput = randomFactResponse + factToTell + this.t("FACT_REPROMPT");
+
+        // this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), factToTell)
+        this.emit(':askWithCard', speechOutput, this.t("FACT_REPROMPT"), this.t("SKILL_NAME"), factToTell)
+        
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = this.t("HELP_MESSAGE");
+        var reprompt = this.t("HELP_MESSAGE");
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    }
+};
+
+function randomPhrase(phraseArr) {
+    // returns a random phrase
+    // where phraseArr is an array of string phrases
+    var i = 0;
+    i = Math.floor(Math.random() * phraseArr.length);
+    return (phraseArr[i]);
+};
+
